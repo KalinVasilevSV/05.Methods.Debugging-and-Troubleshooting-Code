f97e840bad63bd4998ea72ff981a959ba3f5484a
KalinVasilevSV <kalinsv@abv.bg>
2017-10-08 02:16:16 +0300

    commit

---
+++ 02.Hello-Name/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Hello_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+
+            Hello(name); 
+
+        }
+
+        static void Hello(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
